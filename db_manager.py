c90a50a590e1cf536565c95b65bc8d97aa77ecc3
Smit Patel <smitpatel9354@gmail.com>
2023-09-12 18:54:18 -0400

    refactoring for flask and html files

---
+++ db_manager.py
@@ -0,0 +1,77 @@
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+import psycopg2
+import config  # import config.py
+
+
+sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=config.SPOTIFY_CLIENT_ID, client_secret=config.SPOTIFY_CLIENT_SECRET))
+
+
+def connect_to_database():
+    conn = psycopg2.connect(
+        host=config.DB_HOST,
+        database=config.DB_NAME,
+        user=config.DB_USER,
+        password=config.DB_PASSWORD,
+        port=config.DB_PORT
+    )
+    return conn, conn.cursor()
+
+
+def close_database(conn, cur):
+    cur.close()
+    conn.close()
+
+
+def clear_songs_table(cur, conn):
+    cur.execute("TRUNCATE TABLE songs;")
+    conn.commit()
+
+
+def remove_duplicates(cur, conn):
+    cur.execute("""
+    DELETE FROM songs
+    WHERE id NOT IN (
+       SELECT MIN(id)
+        FROM songs
+        GROUP BY name
+    );
+    """)
+    conn.commit()
+
+
+def fetch_latest_songs(cur, limit=1000):
+    cur.execute(f"SELECT * FROM songs ORDER BY id DESC LIMIT {limit};")
+    return cur.fetchall()
+
+
+def get_total_song_count(cur):
+    cur.execute("SELECT COUNT(*) FROM songs;")
+    return cur.fetchone()[0]
+
+
+def get_unique_song_count_by_name(cur):
+    cur.execute("SELECT COUNT(DISTINCT name) FROM songs;")
+    return cur.fetchone()[0]
+
+
+if __name__ == "__main__":
+    conn, cur = connect_to_database()
+
+    # Uncomment the next line if you want to clear the song table:
+    # clear_songs_table(cur, conn)
+
+    # Uncomment next line to remove duplicates
+    # remove_duplicates(cur, conn)
+
+    latest_songs = fetch_latest_songs(cur)
+    for song in latest_songs:
+        print(f"ID: {song[0]}, Name: {song[1]}, Artist: {song[2]}, Danceability: {song[3]}, Energy: {song[4]}, Tempo: {song[5]}, Acousticness: {song[6]}, Instrumentalness: {song[7]}, Valence: {song[8]}")
+
+    total_songs = get_total_song_count(cur)
+    print(f"The total number of songs in the database is: {total_songs}")
+
+    unique_songs = get_unique_song_count_by_name(cur)
+    print(f"There are {unique_songs} unique songs by name in the database.")
+
+    close_database(conn, cur)
---
+++ spotify_api_song_collection.py
@@ -0,0 +1,101 @@
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+import psycopg2
+import config  # Import configuration 
+
+sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=config.SPOTIFY_CLIENT_ID, client_secret=config.SPOTIFY_CLIENT_SECRET))
+
+def connect_to_database():
+    conn = psycopg2.connect(
+        host=config.DB_HOST,
+        database=config.DB_NAME,
+        user=config.DB_USER,
+        password=config.DB_PASSWORD,
+        port=config.DB_PORT
+    )
+    return conn
+
+def create_song_table(conn):
+    cur = conn.cursor()
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS songs (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(255),
+        artist VARCHAR(255),
+        danceability FLOAT,
+        energy FLOAT,
+        tempo FLOAT,
+        acousticness FLOAT,
+        instrumentalness FLOAT,
+        valence FLOAT
+    );
+    """)
+    conn.commit()
+    cur.close()
+
+def fetch_and_insert_songs(total_songs_to_fetch=1000, genre="pop", limit=50):
+    offset = 0  # Starting index of results
+    unique_song_ids = set()
+
+    conn = connect_to_database()
+    cur = conn.cursor()
+
+    while offset < total_songs_to_fetch:
+        track_results = sp.search(q=f'genre:"{genre}"', type='track', limit=limit, offset=offset) 
+
+        for item in track_results['tracks']['items']:
+            song_id = item['id']
+            
+            if song_id in unique_song_ids:
+                continue
+
+            unique_song_ids.add(song_id)
+            song_name = item['name']
+            song_artist = item['artists'][0]['name']
+            features = sp.audio_features(song_id)[0]
+
+            danceability = features['danceability']
+            energy = features['energy']
+            tempo = features['tempo']
+            acousticness = features['acousticness']
+            instrumentalness = features['instrumentalness']
+            valence = features['valence']
+
+            cur.execute("""
+            INSERT INTO songs (name, artist, danceability, energy, tempo, acousticness, instrumentalness, valence)
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s);
+            """, (song_name, song_artist, danceability, energy, tempo, acousticness, instrumentalness, valence))
+
+        conn.commit()
+        offset += limit
+
+    cur.close()
+    conn.close()
+
+def get_total_song_count():
+    conn = connect_to_database()
+    cur = conn.cursor()
+    cur.execute("SELECT COUNT(*) FROM songs;")
+    count = cur.fetchone()[0]
+    cur.close()
+    conn.close()
+    return count
+
+def get_unique_song_count_by_name():
+    conn = connect_to_database()
+    cur = conn.cursor()
+    cur.execute("SELECT COUNT(DISTINCT name) FROM songs;")
+    unique_count = cur.fetchone()[0]
+    cur.close()
+    conn.close()
+    return unique_count
+
+if __name__ == "__main__":
+    conn = connect_to_database()
+    create_song_table(conn)
+    conn.close()
+    fetch_and_insert_songs()
+    total_songs = get_total_song_count()
+    unique_songs = get_unique_song_count_by_name()
+    print(f"The total number of songs in the database is: {total_songs}")
+    print(f"The total number of unique songs (by name) in the database is: {unique_songs}")
