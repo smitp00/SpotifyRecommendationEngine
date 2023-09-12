a7ed64ab4b25d642d4eadee9a2da716432e89c5b
Smit Patel <smitpatel9354@gmail.com>
2023-09-12 19:19:09 -0400

    delete this

---
+++ config.py
@@ -0,0 +1,7 @@
+SPOTIFY_CLIENT_ID = "8adbfe1bbb1345bab14433e02f93f630"
+SPOTIFY_CLIENT_SECRET = "a17511ded02e473884662fc2081b211c"
+DB_HOST = "localhost"
+DB_NAME = "spotify_database"
+DB_USER = "smitpatel"
+DB_PASSWORD = "Spate469!-"
+DB_PORT = "5432"
